2b8b8484f30ca0da6211793f7399b93e59e9ec91
Naman <namanb454@gmail.com>
2022-09-21 13:12:17 +0530

    final

--- resume/settings.py
+++ resume/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 from email.message import Message
 import os
 from pathlib import Path
@@ -131,3 +132,4 @@ STATICFILES_DIRS = [
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+django_heroku.settings(locals())
